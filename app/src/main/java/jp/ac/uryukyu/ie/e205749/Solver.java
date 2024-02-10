c83f609a7bf01b0af41337554fe865a8b8d4d3af
Taisuke Oda <e205749@ie.u-ryukyu.ac.jp>
2024-02-10 20:07:00 +0900

    Write documents

--- app/src/main/java/jp/ac/uryukyu/ie/e205749/Solver.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205749/Solver.java
@@ -3,21 +3,30 @@ package jp.ac.uryukyu.ie.e205749;
 import java.util.Random;
 import java.util.Scanner;
 
+/** このクラスはCUIじゃんけんゲームを実現する． */
+
 public class Solver {
     private Random random;
 
+    /* Solverクラスのコンストラクタ */
     public Solver() {
         this.random = new Random();
     }
 
-    /* CPUが手を選択する． */
+    /** 
+     * CPUが手を選択する． 
+     * @return CPUの手を表す整数(0,1,2)
+     */
     public int getCPUChoice() {
         int CPUchoice = this.random.nextInt(3);
         //System.out.println("CPUの選んだ値:" + CPUchoice);
         return CPUchoice;
     }
 
-    /* CPUが選んだ手を出力する． */
+    /**
+     * CPUが選んだ手を出力する． (グー:0 チョキ:1 パー:2)
+     * @param CPUchoice getCPUChoiceで選択された整数(0,1,2)
+     */
     public void printCPUChoice(int CPUchoice) {
         if (CPUchoice == 0) {
             System.out.println("CPUはグーを出した!");
@@ -30,7 +39,11 @@ public class Solver {
         }
     }
 
-    /* プレイヤーに自らの手を選択させる． */
+    /**
+     * プレイヤーに自らの手を選択させる．
+     * @param scanner 入力を受け付けるScannerオブジェクト
+     * @return 入力された整数 (0,1,2)
+     */
     public int getPlayerChoice(Scanner scanner) {
         System.out.println("グー:0 チョキ:1 パー:2");
         int Pchoice;
@@ -51,7 +64,10 @@ public class Solver {
         return Pchoice;
     }
 
-    /* プレイヤーの選択した手を出力する． */
+    /**
+     * プレイヤーの選択した手を出力する． 
+     * @param Pchoice プレイやーが選択した整数 (0,1,2)
+     */
     public void printPlayerChoice(int Pchoice) {
         if (Pchoice == 0) {
             System.out.println("あなたはグーを出した！");
@@ -64,7 +80,11 @@ public class Solver {
         }
     }
 
-    /* 勝敗判定 */
+    /**
+     * 勝敗判定を行う．
+     * @param Pchoice プレイヤーが選択した整数 (0,1,2)
+     * @param CPUchoice CPUが選択した整数 (0,1,2)
+     */
     public void judge(int Pchoice, int CPUchoice) {
         int twoORone = ( Pchoice - CPUchoice + 3 ) % 3;
         if ( Pchoice == CPUchoice ) {
